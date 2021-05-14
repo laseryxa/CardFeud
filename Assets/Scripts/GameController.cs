3ba5e3aeb25b945309298b1c99fcdd71073bc027
Fredrik Sandstrom <fredrik@xingxiong.se>
2021-05-14 19:10:00 +0200

    Add new cards.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Ability
+{
+    private string name;
+    public Ability(string name)
+    {
+        this.name = name;
+    }
+}
+
 public class GameController : MonoBehaviour
 {
     [SerializeField] public Card cardPrefab;
@@ -16,6 +25,12 @@ public class GameController : MonoBehaviour
     public Player player;
 
     public int turn;
+
+    public List<Ability> abilities;
+
+    public void AddAbility(string name) {
+        abilities.Add(new Ability(name));
+    }
 
     void Awake()
     {
@@ -43,6 +58,16 @@ public class GameController : MonoBehaviour
         playerDeck.CardPrefab = cardPrefab;
         playerHand.deck = playerDeck;
 
+        playerDeck.AddCard("Dogdas", 6, 3, 7); // beast
+        playerDeck.AddCard("Wolf", 2, 2, 2); // beast
+        playerDeck.AddCard("Spirit Wolf", 5, 3, 4); // beast
+        playerDeck.AddCard("Sharp Tooth", 7, 3, 4); // beast
+        playerDeck.AddCard("Uldrurth", 8, 4, 6); // dragon
+        playerDeck.AddCard("Rhinium", 9, 2, 12); // beast
+        playerDeck.AddCard("Rexossum", 7, 6, 9); // beast
+        playerDeck.AddCard("Aarbrok", 3, 2, 4); // beast
+        playerDeck.AddCard("Aluxious", 2, 3, 2); // beast
+        playerDeck.AddCard("Gereon", 7, 6, 8); // dragon
         playerDeck.AddCard("Best card", 0, 10, 10);
         playerDeck.AddCard("Silly card", 3, 0, 3);
         playerDeck.AddCard("Not so good card", 7, 1, 1);
